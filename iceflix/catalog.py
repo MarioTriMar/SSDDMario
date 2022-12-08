1c20ab1259f423c5d2d111fff96c167a654f08b1
MarioTriMar <MarioJose.Trivino@alu.uclm.es>
2022-12-08 19:15:21 +0100

    limpia

--- iceflix/catalog.py
+++ iceflix/catalog.py
@@ -207,16 +207,13 @@ class Catalog(Ice.Application):
         print(proxy)
         
         #Principal proxy
-        
         proxy_principal = self.communicator().propertyToProxy("MainProxy.Proxy")
-        
-        
         principal_prx = IceFlix.MainPrx.checkedCast(proxy_principal)
         if not principal_prx:
             raise RuntimeError('Invalid proxy')
         principal_prx.newService(proxy, str(proxy.ice_getIdentity().name))
-
         self.servant.principal=principal_prx
+
         #Announce Handler
         timer = threading.Timer(25,announce_catalog,[principal_prx,proxy,str(proxy.ice_getIdentity().name)])
         timer.start()
